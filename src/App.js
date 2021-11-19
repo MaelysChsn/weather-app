4ae4d7c5c55ac5ee6ccfef94d2b3207ccb4da27d
Maelys chassin <m.chassin@altelis.com>
2021-11-19 08:55:20 +0100

    Set up Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import DataFetch from './dataFetch.js';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [inputSearch, setInputSearch] = useState([]);
+  const [search, setSearch] = useState([]);
+  const [filterDisplay, setFilterDisplay] = React.useState([]);
+  var view;
+  var darkMode = false;
+
+  const getAPI = (town) => {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=236efda6221d4b70885210424211511&q=${town}&days=7&aqi=no&alerts=no`)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        console.log('success');
+      });
+  };
+
+  useEffect(() => {
+
+    setInputSearch(inputSearch)
+    setSearch(inputSearch);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputSearch, search]);
+
+
+
+  if(data.length !== 0){
+    view = <DataFetch data={data}/>
+    if(data.current.is_day === 0){
+      darkMode = true
+    }
+  }else{
+    view = <p>Veuillez choisir une ville</p>
+  }
+
+ function handleSearch(){
+    setSearch(inputSearch);
+    console.log('search', search);
+    getAPI(search);
+
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${darkMode ? 'theme-dark' : 'theme-light'}`}>
+      <div className="c-search_bar">
+        <span className="c-search_bar-icon"></span>
+        <input className="search-bar" type="text" placeholder="Recherche" value={inputSearch} onChange={e => setInputSearch(e.target.value)}/>
+        <button type="button" onClick={handleSearch}>Rechercher</button>
+      </div>
+
+      {view}
     </div>
   );
 }
---
+++ src/dataFetch.js
@@ -0,0 +1,120 @@
+import React, {useState} from 'react';
+import waterdrop from './img/water-drop.svg';
+import rays from './img/rays.svg';
+import wind from './img/wind.svg';
+
+function DataFetch({data}) {
+  var today = new Date();
+  const [dayWeather, setDayWeather] = useState([]);
+  var today_day = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+
+  var day = [];
+  var weeks = [];
+
+  data.forecast.forecastday.map((item) => {
+    var number = today.getHours();
+    if(today.getHours().toString().length === 1){
+      number = '0'+ today.getHours().toString();
+    }
+
+    if(today_day === item.date){
+      item.hour.map(element => {
+        if(element.time.substr(-5, 2) >= number){
+          day.push(element)
+        }
+      })
+    }else{
+      weeks.push(item)
+    }
+
+    if(day.length < 24){
+      if(item.date === today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate()+1)){
+        item.hour.map(element => {
+          if(day.length < 24){
+            day.push(element)
+          }
+        })
+      }
+    }
+
+    console.log(day.length);
+  })
+
+  return (
+      <div className="main-infos">
+        <div className="min-infos">
+            <div className="">
+              <p>{data.location.name}, {data.location.country}</p>
+              <h1>{data.current.temp_c}°C</h1>
+              <span>{data.current.condition.text}</span>
+            </div>
+            <div className="img-bg"></div>
+          </div>
+
+          <div className="icon-infos">
+            <div className="c-icon_card">
+              <img src={wind} width="100px" alt=""/>
+              <p>{data.current.wind_kph} km/h</p>
+            </div>
+            <div className="c-icon_card">
+              <img src={waterdrop} width="100px" alt=""/>
+              <p>{data.current.humidity} %</p>
+            </div>
+            <div className="c-icon_card">
+              <img src={rays} width="100px" alt=""/>
+              <p>{data.current.uv}</p>
+            </div>
+          </div>
+
+          <div className="day-infos">
+            <p>Today</p>
+            <div className="infos-hours">
+              {
+                day.map(item => {
+                  console.log(item);
+                  var number = today.getHours();
+                  if(today.getHours().toString().length === 1){
+                    number = '0'+ today.getHours().toString();
+                  }
+                  if(item.time.substr(-5, 2) >= number){
+                    return (
+                      <div className="hour-weather">
+                        <p>{item.time.substr(-5, 2)}h</p>
+                        <img src={item.condition.icon}/>
+                        <p>{Math.round(item.temp_c)}°</p>
+                      </div>
+                    )
+                  }
+                  return null;
+                })
+              }
+            </div>
+          </div>
+
+          <div className="week-infos">
+            <p>Météo de la semaine</p>
+            <div className="infos-hours">
+              {
+                weeks.map(item => {
+                  let date = new Date(item.date);
+                  let day = date.toLocaleString('fr-FR', {weekday: 'long'});
+                  return (
+                    <div className="hour-weather">
+                      <p>{day}</p>
+                      <img src={item.day.condition.icon}/>
+                      <p>{Math.round(item.day.mintemp_c)}° / {Math.round(item.day.maxtemp_c)}°</p>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+      </div>
+
+
+
+  );
+}
+
+export default DataFetch;
